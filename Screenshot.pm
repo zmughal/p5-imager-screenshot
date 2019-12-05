13ede9f88b3365296abbf5f154529d485ba880a4
Tony Cook <tony@develop-help.com>
2019-12-05 10:27:08 +0000

    0.014 release

--- Screenshot.pm
+++ Screenshot.pm
@@ -10,7 +10,7 @@ push @ISA, 'Exporter';
 BEGIN {
   require Exporter;
   @ISA = qw(Exporter);
-  $VERSION = '0.013';
+  $VERSION = '0.014';
 
   require XSLoader;
   XSLoader::load('Imager::Screenshot' => $VERSION);
